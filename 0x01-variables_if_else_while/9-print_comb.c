9468b1407ec393885ab8a36d98ad54bb673d735c
Girmaddawit <girmadawitk@gmail.com>
2022-04-11 15:20:03 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success/correct)
+*/
+int main(void)
+
+{
+int c;
+
+for (c = 48; c <=57; c++)
+{
+putchar(c);
+
+if (c == 57)
+{
+break;
+}
+
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+
+}
