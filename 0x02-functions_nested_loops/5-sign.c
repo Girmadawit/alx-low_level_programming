37ea79d778112996920946cccfe5d342c14cca88
Girmaddawit <girmadawitk@gmail.com>
2022-04-13 14:31:54 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
-* _isalpha - checks for alphabetic character
+  * print_sign - prints the sign of a number.
 * @c: takes in a character
-* print_sign - prints the sign of a number
 * Return: 1 if the number is greater than zero, 0 if zero and -1 otherwise
 */
 int print_sign(int c)
