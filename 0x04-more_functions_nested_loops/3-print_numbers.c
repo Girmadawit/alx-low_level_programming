0dfd616810338dc05697d200026963d99cfb93cf
Girmaddawit <girmadawitk@gmail.com>
2022-04-14 12:29:00 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_numbers - prints 0 to 9
+*
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+
+int i;
+
+i = 0;
+
+for (i = 0; i < 10; i++)
+
+_putchar(i + '0');
+
+_putchar('\n');
+}
